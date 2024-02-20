1baa43864a831dd24c3dd2118e6940abed02e2da
pfr2102 <pfrs2102@gmail.com>
2024-02-20 00:08:39 -0700

    usuarios estable final

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -4,8 +4,9 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username', 'email', 'first_name', 
-                  'last_name', 'password', 'is_active', 'is_staff', 'puesto', 'edad', 'antiguedad', 'rango' ]
+        fields = ['id','username', 'first_name', 
+                  'last_name', 'password', 'is_active', 'is_staff', 'nombre', 'dependencia', 'puesto', 'edad', 'antiguedad', 'rango' ]
+        #nota: el username se usa para numero_empleado
 
         
         """ fields = ['id', 'username', 'email', 'first_name', 
