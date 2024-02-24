f58efcdaf92e566641448d207f875d4d4ad76cda
pfr2102 <pfrs2102@gmail.com>
2024-02-24 16:47:02 -0700

    agregacion de consultas especificas en voto

--- voto/api/views.py
+++ voto/api/views.py
@@ -19,9 +19,24 @@ class VotoApiViewSet(ModelViewSet):
     queryset = Voto.objects.all()
     serializer_class = VotoSerializer
 
+
+#-------------------------------------------------------------------------------------------------------------------
     #Peticion para crear votos de forma masiva
     @action(detail=False, methods=['POST'])
     def create_votosM(self, request, *args, **kwargs):
+        """
+        Crea votos de forma masiva.
+
+        Permite crear votos en grandes cantidades proporcionando una lista de datos de votos.
+        ---
+        # Formato de Entrada
+        - Se espera una solicitud POST con una lista de datos de votos.
+        - Cada dato de voto debe tener el formato requerido por el serializador VotoSerializer.
+
+        # Formato de Salida
+        - En caso de éxito, retorna una respuesta con los datos de los votos creados y el código de estado 201 (Created).
+        - En caso de error, retorna los errores de validación con el código de estado 400 (Bad Request).
+        """
         # Utiliza el serializador VotoSerializer en lugar de VotoListSerializer
         serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
         if serializer.is_valid():
@@ -31,17 +46,31 @@ class VotoApiViewSet(ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    #Peticion para obtener el total de votos
+
+#-------------------------------------------------------------------------------------------------------------------
+    #Peticion para obtener el total de votos por rango , etapa y fecha (fecha es el año)
     @swagger_auto_schema(
         manual_parameters=[
             openapi.Parameter('id_etapa_fk', openapi.IN_QUERY, description="Número de etapa", type=openapi.TYPE_STRING),
             openapi.Parameter('id_rango_fk', openapi.IN_QUERY, description="Número de rango", type=openapi.TYPE_STRING),
-            openapi.Parameter('fecha_voto', openapi.IN_QUERY, description="Fecha de voto (opcional)", type=openapi.TYPE_STRING, format='date'),
+            openapi.Parameter('fecha_voto', openapi.IN_QUERY, description="Año voto", type=openapi.TYPE_STRING, format='date'),
         ],
         responses={200: VotoSerializer(many=True)},
     )
     @action(detail=False, methods=['GET'])
     def count_votes(self, request):
+        """
+        Obtiene el total de votos por rango, etapa y fecha.
+
+        ---
+        # Parámetros:
+        - id_etapa_fk: Número de etapa.
+        - id_rango_fk: Número de rango.
+        - fecha_voto: Fecha de voto en formato de año.
+    
+        # Retorna:
+        - una lista de la cantidad de votos por electo con información detallada de mayor a menor.
+        """
         id_etapa_fk = request.query_params.get('id_etapa_fk')
         id_rango_fk = request.query_params.get('id_rango_fk')
         fecha_voto_str = request.query_params.get('fecha_voto')
@@ -77,23 +106,38 @@ class VotoApiViewSet(ModelViewSet):
             .order_by('-total')
         )
 
-
         return Response(counts, status=status.HTTP_200_OK)
+        
 
-    #Peticion para obtener el total de votos
+#-------------------------------------------------------------------------------------------------------------------
+     # Peticion para obtener el total de votos por rango, etapa y fecha (fecha es el año)
     @swagger_auto_schema(
-    manual_parameters=[
-        openapi.Parameter('id_etapa_fk', openapi.IN_QUERY, description="Número de etapa", type=openapi.TYPE_STRING),
-        openapi.Parameter('id_rango_fk', openapi.IN_QUERY, description="Número de rango", type=openapi.TYPE_STRING),
-        openapi.Parameter('fecha_voto', openapi.IN_QUERY, description="Fecha de voto (opcional)", type=openapi.TYPE_STRING, format='date'),
-    ],
-    responses={200: VotoSerializer(many=True)},
+        manual_parameters=[
+            openapi.Parameter('id_etapa_fk', openapi.IN_QUERY, description="Número de etapa", type=openapi.TYPE_STRING),
+            openapi.Parameter('id_rango_fk', openapi.IN_QUERY, description="Número de rango", type=openapi.TYPE_STRING),
+            openapi.Parameter('fecha_voto', openapi.IN_QUERY, description="Año voto", type=openapi.TYPE_STRING, format='date'),
+            openapi.Parameter('top', openapi.IN_QUERY, description="Número de registros a retornar", type=openapi.TYPE_INTEGER),
+        ],
+        responses={200: VotoSerializer(many=True)},
     )
     @action(detail=False, methods=['GET'])
     def count_votesTop(self, request):
+        """
+        Obtiene el total de votos por rango, etapa y año, limitado por un Tope de registros.
+
+        # Parámetros:
+        - id_etapa_fk: Número de etapa.
+        - id_rango_fk: Número de rango.
+        - fecha_voto: Año de voto.
+        - top: Número de registros a retornar (opcional).
+
+        # Retorna:
+        - Lista de votos con información detallada, ordenada por el total de votos de mayor a menor.
+        """
         id_etapa_fk = request.query_params.get('id_etapa_fk')
         id_rango_fk = request.query_params.get('id_rango_fk')
         fecha_voto_str = request.query_params.get('fecha_voto')
+        top_count = request.query_params.get('top')  # Obtener el parámetro "top"
 
         filters = {'estatus_revocacion': False}
 
@@ -109,12 +153,54 @@ class VotoApiViewSet(ModelViewSet):
         counts = (
             Voto.objects
             .filter(**filters)
-            .values('id_etapa_fk', 'id_rango_fk', 'id_emp_candidato_fk' ) 
-            .annotate( full_name=Concat('id_emp_candidato_fk_first_name', Value(' '), 'id_emp_candidato_fk_last_name'), year=TruncYear('fecha_voto'), total=Count('id_voto'))  
-            .order_by('-total')
-        )[:6]
-
+            .values(
+                    'id_etapa_fk',
+                    'id_rango_fk', 
+                    'id_emp_candidato_fk', 
+                    image=F('id_emp_candidato_fk__image'), 
+                    workstation=F('id_emp_candidato_fk__workstation'),
+                    dependency=F('id_emp_candidato_fk__dependency'),
+                    num_empleado=F('id_emp_candidato_fk__username') 
+                 ) 
+            .annotate( 
+                    full_name=Concat('id_emp_candidato_fk__first_name', Value(' '), 'id_emp_candidato_fk__last_name'),                 
+                    year=TruncYear('fecha_voto'), 
+                    total=Count('id_voto')
+                )   
+            .order_by('-total')[:int(top_count)] if top_count else None  # Aplicar el recuento superior si se proporciona
+        )
 
         return Response(counts, status=status.HTTP_200_OK)
 
+#-------------------------------------------------------------------------------------------------------------------
+    # Acción para verificar la existencia de registros con la fk, la etapa y el año proporcionados
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('id_emp_votante_fk', openapi.IN_QUERY, description="Número de empleado del votante", type=openapi.TYPE_STRING),
+            openapi.Parameter('id_etapa_fk', openapi.IN_QUERY, description="Número de etapa", type=openapi.TYPE_STRING),
+            openapi.Parameter('fecha_voto', openapi.IN_QUERY, description="Año voto", type=openapi.TYPE_STRING, format='date'),
+        ],
+        responses={200: openapi.TYPE_BOOLEAN},  # Cambiado a openapi.TYPE_BOOLEAN
+    )
+    @action(detail=False, methods=['GET'])
+    def check_vote_existence(self, request):
+        """
+        Verifica la existencia de un voto basado en el número de empleado del votante, el número de etapa y el año de voto.
 
+        # Parámetros:
+        - id_emp_votante_fk: Número de empleado del votante.
+        - id_etapa_fk: Número de etapa.
+        - fecha_voto: Año de voto.
+
+        # Retorna:
+        - True si existe un voto con las condiciones dadas, False de lo contrario.
+        """
+        id_emp_votante_fk = request.query_params.get('id_emp_votante_fk')
+        id_etapa_fk = request.query_params.get('id_etapa_fk')
+        fecha_voto_str = request.query_params.get('fecha_voto')
+
+        filters = {'id_emp_votante_fk': id_emp_votante_fk, 'id_etapa_fk': id_etapa_fk, 'fecha_voto__year': fecha_voto_str}
+
+        exists = Voto.objects.filter(**filters).exists()
+
+        return Response(exists, status=status.HTTP_200_OK)
