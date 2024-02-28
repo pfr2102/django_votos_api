e4a058a550ad5142473180e98c34e44640d1b99c
pfr2102 <pfrs2102@gmail.com>
2024-02-28 12:56:21 -0700

    actualizacion de requirements.txt

--- icard/settings.py
+++ icard/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'icard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'VOTOS_COMITE',
+        'USER': 'postgres',
+        'PASSWORD': '2102',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
